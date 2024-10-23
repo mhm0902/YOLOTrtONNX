a4a51fb49d535b77d469419bcb4bb8e1ccb48d2e
马洪民 <mhm0902@163.com>
2024-10-23 16:18:21 +0800

    add

---
+++ build/CMakeFiles/yolo11_det.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolo11_det.dir/src/postprocess.cpp.o"
+  "CMakeFiles/yolo11_det.dir/src/postprocess.cpp.o.d"
+  "CMakeFiles/yolo11_det.dir/src/postprocess.cu.o"
+  "CMakeFiles/yolo11_det.dir/src/postprocess.cu.o.d"
+  "CMakeFiles/yolo11_det.dir/src/preprocess.cu.o"
+  "CMakeFiles/yolo11_det.dir/src/preprocess.cu.o.d"
+  "CMakeFiles/yolo11_det.dir/src/yolo11_det.cpp.o"
+  "CMakeFiles/yolo11_det.dir/src/yolo11_det.cpp.o.d"
+  "yolo11_det"
+  "yolo11_det.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/yolo11_det.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
